597aeb5ff494b64c64a72ef07d8ef7b92c1fa2c3
Filipe de Medeiros Santos <filipemedeiros2013@gmail.com>
2023-01-02 13:01:32 -0300

    Adicionando legenda para referenciar dados brutos

--- observatorio.py
+++ observatorio.py
@@ -34,22 +34,24 @@ def carregar_dados(base_dados):
     exame = pd.DataFrame(res.fetchall(),
                          columns=np.array(res.description)[:, 0])
     exame.drop(columns=["index"], inplace=True)
+    
+    # Fechar conexão
 
     return tabela_fato, escolas, faixa_etaria, exame
 
 # Cabeçalho inicial
-st.title('Observatório do Sorriso')
+st.title("Observatório do Sorriso")
 
 # Carregando dados
 fato, escolas, fe, exame = carregar_dados('observatorio_sorriso')
 
+# Quantidade de alunos por região
 st.header("Quantidade de alunos examinados em Palmas-TO")
 
-# Quantidade de alunos por região
-alunos_regiao = fato.join(escolas["escola.região"], on="escola_id").groupby("escola.região")["quantidade_populacao"].sum()
+alunos_regiao = fato.join(escolas["escola.região"], on="escola_id")
+alunos_regiao = alunos_regiao.groupby("escola.região")["quantidade_populacao"].sum()
 
-fig = px.pie(alunos_regiao, values="quantidade_populacao",
-             names=alunos_regiao.index, hole=.7)
+fig = px.pie(alunos_regiao, values="quantidade_populacao", names=alunos_regiao.index, hole=.7)
 fig.update_layout(annotations=[dict(text=f"<b>{alunos_regiao.sum()}</b>", x=0.5, y=0.55, font_size=30, showarrow=False),
                                dict(text="alunos", x=0.5, y=0.45, font_size=20, showarrow=False)])
 st.plotly_chart(fig)
@@ -61,17 +63,8 @@ col1, col2, col3, col4 = st.columns(4)
 
 col1.metric("CPO-D Palmas", np.round(fato["soma_cpo"].sum() / fato["quantidade_populacao"].sum(), 2))
 col2.metric("escolas públicas", escolas.shape[0])
-col3.metric("territórios", escolas["escola.território"].unique().shape[0])
-col4.metric("regiões", escolas["escola.região"].unique().shape[0])
+col3.metric("territórios", escolas["escola.território"].nunique())
+col4.metric("regiões", escolas["escola.região"].nunique())
 
-# Obtendo os dados brutos
-st.selectbox("Exibir tabela:", ("", "CPO-D", "Escolas", "Faixa_etaria", "Exame"), key="exibir_tabelas")
-
-if st.session_state.exibir_tabelas == "CPO-D":
-    st.dataframe(fato)
-elif st.session_state.exibir_tabelas == "Escolas":
-    st.dataframe(escolas)
-elif st.session_state.exibir_tabelas == "Faixa_etaria":
-    st.dataframe(fe)
-elif st.session_state.exibir_tabelas == "Exame":
-    st.dataframe(exame)
+# Referências dos dados brutos
+st.caption("Dados brutos disponíveis na [página do github](https://github.com/filipemedeiross/observatorio_sorriso/tree/main/dados) do projeto.")
